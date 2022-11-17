44619beaa79cf4428ce4ed3f92696f425e4c9fd6
yega <egor.grigolenok@mail.ru>
2022-11-17 11:42:13 +0300

    first commit

---
+++ src/main/java/com/example/dockertutor/DockerTutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockertutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTutorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockertutor/DockerTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockertutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
